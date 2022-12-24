5e6f00c691069da2efc334304889a7180b397ad9
Txell Amigó <txellmgsol@gmail.com>
2022-12-24 17:27:20 +0100

    8 kyu created, playing banjo done

---
+++ 8_kyu/Are you playing banjo/index.js
@@ -0,0 +1,42 @@
+/*
+Title:
+    Are you playing Banjo?
+
+Description:
+    Create a function which answers the question "Are you playing banjo?".
+    If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+    The function takes a name as its only argument, and returns one of the following strings:
+
+    name + " plays banjo" 
+    name + " does not play banjo"
+    Names given are always valid strings.
+
+
+*******************************************************************************
+Kata link:
+    https://www.codewars.com/kata/53af2b8861023f1d88000832
+
+Discuss link: 
+    https://www.codewars.com/kata/53af2b8861023f1d88000832/discuss
+
+Solutions link:
+    https://www.codewars.com/kata/53af2b8861023f1d88000832/solutions/javascript
+
+********************************************************************************
+*/
+
+
+// Solution
+function areYouPlayingBanjo(name) {
+    for (let letter of name) {
+      if (letter == "R" || letter == "r") {
+        return name + " plays banjo";
+      } else {
+        return name + " does not play banjo";
+      }
+    }
+  }
+
+  // Function Export
+  module.export = areYouPlayingBanjo
---
+++ 8_kyu/Are you playing banjo/test.js
@@ -0,0 +1,13 @@
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(areYouPlayingBanjo("Adam"), "Adam does not play banjo");
+    assert.strictEqual(areYouPlayingBanjo("Paul"), "Paul does not play banjo");
+    assert.strictEqual(areYouPlayingBanjo("Ringo"), "Ringo plays banjo");
+    assert.strictEqual(areYouPlayingBanjo("bravo"), "bravo does not play banjo");
+    assert.strictEqual(areYouPlayingBanjo("rolf"), "rolf plays banjo");
+    })
+  })
